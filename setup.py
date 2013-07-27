a9322aab106f559ef214723cc4af7a9906cae7c9
Andre Sencioles <asenci@gmail.com>
2013-07-27 04:38:14 -0300

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='fillrandom',
+    version='0.1',
+    packages=[''],
+    py_modules=['fillrandom'],
+    scripts=['fillrandom.py'],
+    url='https://bitbucket.org/asenci/fillrandom',
+    license='ISC License',
+    author='Andre Sencioles Vitorio Oliveira',
+    author_email='andre@bcp.net.br',
+    description='Copy random files from source to destination'
+)
